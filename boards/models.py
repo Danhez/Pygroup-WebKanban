be0223205d5ac9226bc2fd1855b709a90f7b4fcd
danhez <danionatee@gmail.com>
2022-03-15 18:15:25 -0500

    Models Check

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField("Name", max_length=240)
+    email = models.EmailField()
+    avatar = models.URLField()
+
+
+class Board(models.Model):
+    name = models.CharField("Name", max_length=25)
+
+
+class Column(models.Model):
+    name = models.CharField("Name", max_length=25)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    PRIORITIES = (
+        ('L', 'Low'),
+        ('M', 'Medium'),
+        ('H', 'High'),
+    )
+    description = models.CharField("Description", max_length=250)
+    priority = models.CharField(max_length=1,choices=PRIORITIES)
+    registration_date = models.DateField("Registration Date", auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    column = models.ForeignKey(Column, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
