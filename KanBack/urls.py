2536cc7642afe6cea3b1dfd6484c7278174eacc0
danhez <danionatee@gmail.com>
2022-03-15 19:31:55 -0500

    REST API Ready

--- KanBack/urls.py
+++ KanBack/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from boards import views
+
+router = routers.DefaultRouter()
+router.register(r'boards', views.BoardView, 'board')
+router.register(r'columns', views.ColumnView, 'column')
+router.register(r'users', views.UserView, 'user')
+router.register(r'tasks', views.TaskView, 'task')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BoardSerializer, TaskSerializer, ColumnSerializer, UserSerializer
+from .models import Board, Task, Column, User
 
 # Create your views here.
+
+
+class BoardView(viewsets.ModelViewSet):
+    serializer_class = BoardSerializer
+    queryset = Board.objects.all()
+
+
+class TaskView(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+
+class ColumnView(viewsets.ModelViewSet):
+    serializer_class = ColumnSerializer
+    queryset = Column.objects.all()
+
+
+class UserView(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
