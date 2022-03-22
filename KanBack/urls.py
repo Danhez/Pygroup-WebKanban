abd76c33029c0c4ee69f3baa1a10e2bdabc2e279
Danhez <danionatee@gmail.com>
2022-03-21 20:19:30 -0500

    Better backend

--- KanBack/urls.py
+++ KanBack/urls.py
@@ -14,17 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 from boards import views
 
+"""
 router = routers.DefaultRouter()
-router.register(r'boards', views.BoardView, 'board')
-router.register(r'columns', views.ColumnView, 'column')
-router.register(r'users', views.UserView, 'user')
-router.register(r'tasks', views.TaskView, 'task')
+router.register(r'boards', views.boards_list, 'board')
+router.register(r'boards', views.boards_detail, 'board')
+router.register(r'columns', views.columns_list, 'column')
+router.register(r'columns', views.columns_detail, 'column')
+router.register(r'users', views.users_list, 'user')
+router.register(r'users', views.users_detail, 'user')
+router.register(r'tasks', views.tasks_list, 'task')
+router.register(r'tasks', views.tasks_detail, 'task')
+"""
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/boards/', views.boards_list),
+    path('api/tasks/', views.tasks_list),
+    path('api/columns/', views.columns_list),
+    path('api/users/', views.users_list),
+    path('api/tasks/<int:pk>/', views.tasks_detail),
+    path('api/columns/<int:pk>/', views.columns_detail),
+    path('api/boards/<int:pk>/', views.boards_detail),
+    path('api/users/<int:pk>/', views.users_detail),
 ]
